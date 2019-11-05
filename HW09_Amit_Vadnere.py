b4d7ddb6385cfcf157948d75cf124b034a91cb86
Amit Vadnere <amit.vadnere0@gmail.com>
2019-11-05 12:08:46 -0500

    HW09

---
+++ HW09_Amit_Vadnere.py
@@ -0,0 +1,233 @@
+"""
+Created on 2019-10-31 15:46:23
+@author: Amit Vadnere
+Data repository of courses, students, and instructor
+"""
+import collections
+import os
+from prettytable import PrettyTable
+
+
+class Repository:
+
+    """ Holds the information about the students, instructors and grades for a single University"""
+
+    def __init__(self, university_name, directory, pretty_print):
+
+        """initalize the variables."""
+        self.__university_name = university_name
+        self.__directory = directory
+        self.__student_summary = dict()
+        self.__instructor_summary = dict()
+
+        self.get_student()
+        self.get_instructor()
+        self.get_grade()
+        if pretty_print:
+            print(self.pretty_print_student_summary())
+            print(self.pretty_print_instructor_summary())
+
+    def file_reading_gen(self, path, fields, sep='\t', header=False):
+
+        """ yield a tuple with all of the values from a single line in the file """
+        if not os.path.isfile(path):
+            raise FileNotFoundError(f"cannot open the {path}")
+        fp = open(path, 'r')
+        with fp:
+            for (indx, line) in enumerate(fp):
+                fields_array = line.strip().split(sep)
+                if len(fields_array) != fields:
+                    raise ValueError(f"‘{path}’ has {len(fields_array)} fields on line {indx+1} but expected {fields}")
+                else:
+                    if header:
+                        header = False
+                        continue
+                    else:
+                        yield tuple(i for i in fields_array)
+
+    def get_student(self):
+
+        """ get the student detail"""
+        file_name = os.path.join(self.__directory, "students.txt")
+        try:
+            for student in self.file_reading_gen(file_name, 3, '\t'):
+                self.__student_summary[student[0]] = Student(cwid=student[0], name=student[1], major=student[2])
+
+        except FileNotFoundError as fnfe:
+            print(fnfe)
+
+        except ValueError as ve:
+            print(ve)
+
+        except Exception as error:
+            print(f"File cannot be open {self.__directory} {error}")
+
+    def get_instructor(self):
+
+        """ get the instructor details"""
+        file_name = os.path.join(self.__directory, "instructors.txt")
+        try:
+            for instructor in self.file_reading_gen(file_name, 3, '\t'):
+                self.__instructor_summary[instructor[0]] = Instructor(instructor[0], instructor[1], instructor[2])
+
+        except FileNotFoundError as fnfe:
+            print(fnfe)
+
+        except ValueError as ve:
+            print(ve)
+
+        except Exception as error:
+            print(f"File cannot be open {self.__directory} {error}")
+
+    def get_grade(self):
+
+        """ get the instructor details"""
+        file_name = os.path.join(self.__directory, "grades.txt")
+        try:
+            for student in self.file_reading_gen(file_name, 4, '\t'):
+                student_instance = self.__student_summary[student[0]]
+                instructor_instance = self.__instructor_summary[student[3]]
+                student_instance.add_course_and_grade(student[1], student[2])
+                instructor_instance.add_course(student[1])
+
+        except FileNotFoundError as fnfe:
+            print(fnfe)
+
+        except ValueError as ve:
+            print(ve)
+
+        except Exception as error:
+            print(f"File cannot be open {self.__directory} {error}")
+
+    def pretty_print_student_summary(self):
+
+        """ prints the preety table with student summary"""
+        table = PrettyTable(field_names=Student.FIELD_NAME)
+
+        for student_detail in self.__student_summary.values():
+            pretty_row = student_detail.get_student_row()
+            table.add_row(pretty_row)
+
+        return table
+
+    def pretty_print_instructor_summary(self):
+
+        """ prints the preety table with Instructor summary"""
+        table = PrettyTable(field_names=Instructor.FIELD_NAME)
+
+        for instructor_details in self.__instructor_summary.values():
+            for row in instructor_details.get_instructor_row():
+                table.add_row(row)
+
+        return table
+
+    def get_university_name(self):
+
+        """ return the university name """
+        return self.__university_name
+
+    def get_directory(self):
+
+        """ return the department"""
+        return self.__directory
+
+    def get_instructor_summary(self):
+
+        """ return the list of instructor with id as cwid and instructor object"""
+        return self.__instructor_summary
+
+    def get_student_summary(self):
+
+        """ return the list of student  with id as cwid and student object """
+        return self.__student_summary
+
+class Student:
+
+    "Holds all of the details of a student"
+
+    FIELD_NAME = ['CWID', 'Name', 'Courses']
+
+    def __init__(self, cwid, name, major):
+
+        """ initializing the student variables """
+        self.__name = name
+        self.__major = major
+        self.__cwid = cwid
+        self.__courses = collections.defaultdict(str)
+
+    def add_course_and_grade(self, course, grade):
+
+        """ Store the classes taken and the grade """
+        self.__courses[course] = grade
+
+    def get_student_row(self):
+
+        """ return the student detail for pretty row """
+        courseList = [course for course in self.__courses]
+        pretty_row = (self.__cwid, self.__name, sorted(courseList))
+        return pretty_row
+
+    def get_cwid(self):
+
+        """ return the cwid for student """
+        return self.__cwid
+
+    def get_major(self):
+
+        """ return the major for student """
+        return self.__major
+
+    def get_name(self):
+
+        """ return the courses for student"""
+        return self.__name
+
+    def get_courses(self):
+
+        """ return the courses for student"""
+        return self.__courses
+
+class Instructor:
+
+    "Holds all of the details of a Instructor"
+
+    FIELD_NAME = ['CWID', 'Name', 'Department', 'Course', 'Student']
+
+    def __init__(self, cwid, name, department):
+
+        """ initializing the Instructor variables """
+        self.__cwid = cwid
+        self.__name = name
+        self.__department = department
+        self.__courses = collections.defaultdict(int)
+
+    def add_course(self, course):
+
+        "Store the count of student per course "
+        self.__courses[course] += 1
+
+    def get_instructor_row(self):
+
+        """ return the instructor detail for pretty row """
+        for course_name, enrolment_count in self.__courses.items():
+            yield[self.__cwid, self.__name, self.__department, course_name, enrolment_count]
+
+    def get_cwid(self):
+
+        """ return the cwid for instructor """
+        return self.__cwid
+
+    def get_department(self):
+
+        """ return the department for instructor """
+        return self.__department
+
+    def get_name(self):
+
+        """ return the name for instructor """
+        return self.__name
+
+    def get_courses(self):
+
+        """ return the courses for instructor """
+        return self.__courses
---
+++ HW09_Amit_Vadnere_Test.py
@@ -0,0 +1,98 @@
+"""
+Created on 2019-10-30 17:15:53
+@author: Amit Vadnere
+Test the Data repository of courses, students, and instructor
+"""
+
+import unittest
+import os
+from HW09_Amit_Vadnere import Repository, Student, Instructor
+
+
+class TestRepository(unittest.TestCase):
+
+    """Test the Repository Class"""
+
+    def test_init(self):
+        """ Test the init function """
+        repository = Repository("STEVENS", "F:", False)
+        self.assertEqual(repository.get_directory(),"F:")
+        self.assertEqual(repository.get_university_name(), "STEVENS")
+
+    def test_file_reading_gen(self):
+        """ Reading File Properly"""
+        
+        expected_case_1 = [ ('10103','Baldwin, C', 'SFEN'),
+                            ('10115','Wyatt, X', 'SFEN'),
+                            ('10172','Forbes, I', 'SFEN'),
+                            ('10175','Erickson, D', 'SFEN'),
+                            ('10183','Chapman, O', 'SFEN'),
+                            ('11399','Cordova, I', 'SYEN'),
+                            ('11461','Wright, U', 'SYEN'),
+                            ('11658','Kelly, P', 'SYEN'),
+                            ('11714','Morton, A', 'SYEN'),
+                            ('11788','Fuller, E', 'SYEN'),
+                           ]
+
+        repository = Repository( "STEVENS", os.getcwd(), False)
+        self.assertEqual(list(repository.file_reading_gen(path="students.txt", fields=3)),
+                         expected_case_1)
+        with self.assertRaises(ValueError):
+            list(repository.file_reading_gen(path="students.txt", fields=4))
+            
+        with self.assertRaises(FileNotFoundError):
+            list(repository.file_reading_gen(path="random_file.txt", fields=3, header=True))
+
+    def test_get_student(self):
+
+        """ Reading student file properly """
+        repository = Repository("STEVENS", os.getcwd(), False)
+        student_summary_dict = repository.get_student_summary()
+        student = student_summary_dict["11399"]
+        self.assertEqual(student.get_name(), "Cordova, I")
+        self.assertEqual(student.get_major(), "SYEN")
+
+    def test_get_instructor(self):
+
+        """ Reading Instructor file properly """
+        repository = Repository("STEVENS", os.getcwd(), False)
+        instructor_summary_dict = repository.get_instructor_summary()
+        instructor = instructor_summary_dict["98761"]
+        self.assertEqual(instructor.get_name(), "Edison, A")
+        self.assertEqual(instructor.get_department(), "SYEN")
+
+    def test_get_grade(self):
+    
+        """ Reading and processing grade file properly  """
+        repo = Repository("STEVENS", os.getcwd(), False)
+        student_summary_dict = repo.get_student_summary()
+        student = student_summary_dict["11399"]
+        self.assertEqual(student.get_courses(), {"SSW 540":"B"})
+
+class TestStudent(unittest.TestCase):
+    
+    "Test that it Holds all of the details of a student"
+
+    def Test__init__(self ):
+    
+        """ Test that the student variables are intialized properly """
+        student = Student("10442085", "abc","Medicaps")
+        self.assertEqual(student.get_cwid, "10442085")
+        self.assertEqual(student.get_name(), "abc")
+        self.assertEqual(student.get_major(), "Medicaps")
+
+class TestInstructor(unittest.TestCase):
+    
+    "Test that it Holds all of the details of a Instructor"
+
+    def Test_init__(self):
+    
+        """ Test the Instructor variables are  intialize properly """
+        instructor = Instructor("1042055","abc","Medicaps")
+        self.assertEqual(instructor.get_cwid(), "1042055")
+        self.assertEqual(instructor.get_name(), "abc")
+        self.assertEqual(instructor.get_department(), "Medicaps")
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
+    repo = Repository("NYU", os.getcwd(), True)
